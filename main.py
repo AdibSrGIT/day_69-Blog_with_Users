cc8965f7f60199fef291f64770e289a6ead47de4
AdibSrGIT <102172033+AdibSrGIT@users.noreply.github.com>
2022-10-30 16:55:53 +0600

    Changed DB to only db3

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('sqlite:///blog3.db', 'DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog3.db' # os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
